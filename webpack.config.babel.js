84089ffd4f52b967fa702f5c000588afbae9a8f4
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-06-10 16:19:33 +0200

    add loader for font file in webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -47,6 +47,10 @@ module.exports = {
       {
         test: /\.(png|jpg)$/,
         loader: 'url-loader'
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'url-loader'
       }
     ]
   },
